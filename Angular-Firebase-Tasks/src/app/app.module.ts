2def18913caf453bc0924b5ddc5fdaca8b067c4e
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2019-06-27 20:18:44 -0300

    FirstCommit

---
+++ Angular-Firebase-Tasks/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { environment } from 'src/environments/environment';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ Angular-Firebase-Tasks/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBEegQeFXfgQuLH0Dfp5vmOZGYsBVdZCL8',
+    authDomain: 'angular-tasks-a19e2.firebaseapp.com',
+    databaseURL: 'https://angular-tasks-a19e2.firebaseio.com',
+    projectId: 'angular-tasks-a19e2',
+    storageBucket: '',
+    messagingSenderId: '569898065700',
+    appId: '1:569898065700:web:99a988a46f73807b'
+  }
+};
