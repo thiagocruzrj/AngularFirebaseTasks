2181da3d955a8d11b0076371c075cb41c84aef55
Thiago Cruz <thagocruz@gmail.com>
2020-09-09 00:13:05 -0300

    Update Program.cs

--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -8,5 +8,30 @@ namespace TextEditor
         {
             Console.WriteLine("Hello World!");
         }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("What do you need ? ");
+            Console.WriteLine("1 - Open File ");
+            Console.WriteLine("2 - Create a new file ");
+            Console.WriteLine("0 - Exit ");
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+
+            }
+        }
+
+        static void Open()
+        {
+
+        }
+
+        static void Edit()
+        {
+
+        }
     }
 }
