035108495a5e8dce5866e6e96df3df299987ab75
Thiago Cruz <thagocruz@gmail.com>
2020-09-08 23:26:00 -0300

    Update Program.cs

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -21,7 +21,16 @@ namespace StopWatch
 
             string data = Console.ReadLine().ToLower();
             char type = char.Parse(data.Substring(data.Length - 1, 1));
-            Console.WriteLine(data);
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multMinute = 1;
+
+            if (time == 'm')
+                multMinute = 60;
+
+            if (time == 0)
+                System.Environment.Exit(0);
+
+            Start(time * multMinute);
         }
 
         static void Start(int time)
@@ -38,6 +47,7 @@ namespace StopWatch
             Console.Clear();
             Console.Write("Stopwatch finished");
             Thread.Sleep(2000);
+            Menu();
         }
     }
 }
