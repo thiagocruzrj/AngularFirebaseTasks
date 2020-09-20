55f5377ae88aa94cfc006d4a8557a01ecb6fb80c
Thiago <thagocruz@gmail.com>
2020-09-20 18:16:38 -0300

    Create Viewer.cs

---
+++ HtmlEditor/Viewer.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace HtmlEditor
+{
+    public class Viewer
+    {
+        public static void Show()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("Editor Mode");
+            Console.WriteLine("-----------");
+        }
+    }
+}
