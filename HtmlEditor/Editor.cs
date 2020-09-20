c3d2104c45c575128bf8b7fa4c38ec23817e5b3e
Thiago <thagocruz@gmail.com>
2020-09-20 17:39:43 -0300

    Update Editor.cs

--- HtmlEditor/Editor.cs
+++ HtmlEditor/Editor.cs
@@ -1,10 +1,18 @@
+using System;
+
 namespace HtmlEditor
 {
     public static class Editor
     {
         public static void Show()
         {
-            
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("Editor Mode");
+            Console.WriteLine("-----------");
+            Start();
         }
 
         public static void Start()
