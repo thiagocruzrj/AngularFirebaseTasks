ae3400f8cc0ea2c3dca07ac100eabfc853446b94
Thiago <thagocruz@gmail.com>
2020-09-20 17:44:59 -0300

    Update Menu.cs

--- HtmlEditor/Menu.cs
+++ HtmlEditor/Menu.cs
@@ -64,7 +64,7 @@ namespace HtmlEditor
         {
             switch(option)
             {
-                case 1: Console.WriteLine("Editor"); break;
+                case 1: Editor.Show(); break;
                 case 2: Console.WriteLine("View"); break;
                 case 0: 
                 {
