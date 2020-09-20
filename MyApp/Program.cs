71e7a277ebe3f2273d1b9385a8b6662d637d7ea6
Thiago <thagocruz@gmail.com>
2020-09-19 23:50:57 -0300

    testing guid

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -7,6 +7,8 @@ namespace MyApp
         static void Main(string[] args)
         {
             var id = Guid.NewGuid();
+            id.ToString();
+            id = new Guid("163fde36-c352-418f-85de-4c48663ad786");
             Console.WriteLine(id);
         }
     }
