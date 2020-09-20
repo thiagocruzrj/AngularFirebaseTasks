1bdf033b006d632f769b604a526e47e82e54c8cf
Thiago <thagocruz@gmail.com>
2020-09-20 18:47:53 -0300

    Update Menu.cs

--- HtmlEditor/Menu.cs
+++ HtmlEditor/Menu.cs
@@ -18,60 +18,62 @@ namespace HtmlEditor
 
         public static void DrawScreen()
         {
-            DrawingHeaderFooter();
+            Console.Write("+");
+            for (int i = 0; i <= 38; i++)
+            {
+                Console.Write("-");
+            }
+            Console.Write("+");
+            Console.Write("\n");
 
             for (int lines = 0; lines <= 10; lines++)
             {
                 Console.Write("|");
-                for (int i = 0; i <= 30; i++)
+                for (int i = 0; i <= 38; i++)
+                {
                     Console.Write(" ");
-
+                }
                 Console.Write("|");
                 Console.Write("\n");
             }
-            DrawingHeaderFooter();
+
+            Console.Write("+");
+            for (int i = 0; i <= 38; i++)
+            {
+                Console.Write("-");
+            }
+            Console.Write("+");
         }
 
         public static void WriteOptions()
         {
             Console.SetCursorPosition(3, 2);
-            Console.WriteLine("Html Editor");
+            Console.WriteLine("Editor HTML");
             Console.SetCursorPosition(3, 3);
-            Console.WriteLine("============");
+            Console.WriteLine("===========");
             Console.SetCursorPosition(3, 4);
-            Console.WriteLine("Select a option below");
+            Console.WriteLine("Selecione uma opção abaixo");
             Console.SetCursorPosition(3, 6);
-            Console.WriteLine("1 - New File");
+            Console.WriteLine("1 - Novo arquivo");
             Console.SetCursorPosition(3, 7);
-            Console.WriteLine("2 - Open File");
+            Console.WriteLine("2 - Abrir");
             Console.SetCursorPosition(3, 9);
-            Console.WriteLine("0 - Exit");
+            Console.WriteLine("0 - Sair");
             Console.SetCursorPosition(3, 10);
-            Console.Write("Option: ");
-        }
-
-        private static void DrawingHeaderFooter()
-        {
-            Console.Write("+");
-            for(int i = 0; i <= 30; i++)
-                Console.Write("-");
-            
-            Console.Write("+");
-            Console.Write("\n");
+            Console.Write("Opção: ");
         }
 
         public static void HandleMenuOption(short option)
         {
-            switch(option)
+            switch (option)
             {
                 case 1: Editor.Show(); break;
-                case 2: Console.WriteLine("View"); break;
-                case 0: 
-                {
-                    Console.Clear();
-                    Environment.Exit(0);
-                    break;
-                }
+                case 2: Viewer.Show(""); break;
+                case 0:
+                    {
+                        Console.Clear();
+                        Environment.Exit(0); break;
+                    }
                 default: Show(); break;
             }
         }
