e9de8bcf4323fb2e6c609cd8b9ea67fcd0c19a3a
Thiago <thagocruz@gmail.com>
2020-09-20 16:51:15 -0300

    Update Menu.cs

--- HtmlEditor/Menu.cs
+++ HtmlEditor/Menu.cs
@@ -11,11 +11,18 @@ namespace HtmlEditor
             Console.ForegroundColor = ConsoleColor.Black;
             DrawScreen();
             WriteOptions();
+
+            var option = short.Parse(Console.ReadLine());
         }
 
         public static void DrawScreen()
         {
-            DrawingHeaderFooter();
+            Console.Write("+");
+            for(int i = 0; i <= 30; i++)
+                Console.Write("-");
+            
+            Console.Write("+");
+            Console.Write("\n");
 
             for (int lines = 0; lines <= 10; lines++)
             {
@@ -26,35 +33,31 @@ namespace HtmlEditor
                 Console.Write("|");
                 Console.Write("\n");
             }
-            DrawingHeaderFooter();
-        }
-
-        public static void WriteOptions()
-        {
-            Console.SetCursorPosition(3,2);
-            Console.WriteLine("Html Editor");
-            Console.SetCursorPosition(3,3);
-            Console.WriteLine("============");
-            Console.SetCursorPosition(3,4);
-            Console.WriteLine("Select a option below");
-            Console.SetCursorPosition(3,6);
-            Console.WriteLine("1 - New File");
-            Console.SetCursorPosition(3,7);
-            Console.WriteLine("2 - Open File");
-            Console.SetCursorPosition(3,9);
-            Console.WriteLine("0 - Exit");
-            Console.SetCursorPosition(3,10);
-            Console.Write("Option: ");
-        }
-
-        private static void DrawingHeaderFooter()
-        {
+            
             Console.Write("+");
             for(int i = 0; i <= 30; i++)
                 Console.Write("-");
             
             Console.Write("+");
             Console.Write("\n");
+        }
+
+        public static void WriteOptions()
+        {
+            Console.SetCursorPosition(3, 2);
+            Console.WriteLine("Html Editor");
+            Console.SetCursorPosition(3, 3);
+            Console.WriteLine("============");
+            Console.SetCursorPosition(3, 4);
+            Console.WriteLine("Select a option below");
+            Console.SetCursorPosition(3, 6);
+            Console.WriteLine("1 - New File");
+            Console.SetCursorPosition(3, 7);
+            Console.WriteLine("2 - Open File");
+            Console.SetCursorPosition(3, 9);
+            Console.WriteLine("0 - Exit");
+            Console.SetCursorPosition(3, 10);
+            Console.Write("Option: ");
         }
     }
 }
