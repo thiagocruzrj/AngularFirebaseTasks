de159e4b4913208f4b5e89c53d0d667eb13738d0
Thiago <thagocruz@gmail.com>
2020-09-20 16:47:04 -0300

    Add WriteOptions method

--- HtmlEditor/Menu.cs
+++ HtmlEditor/Menu.cs
@@ -29,6 +29,24 @@ namespace HtmlEditor
             DrawingHeaderFooter();
         }
 
+        public static void WriteOptions()
+        {
+            Console.SetCursorPosition(3,2);
+            Console.WriteLine("Html Editor");
+            Console.SetCursorPosition(3,3);
+            Console.WriteLine("============");
+            Console.SetCursorPosition(3,4);
+            Console.WriteLine("Select a option below");
+            Console.SetCursorPosition(3,6);
+            Console.WriteLine("1 - New File");
+            Console.SetCursorPosition(3,7);
+            Console.WriteLine("2 - Open File");
+            Console.SetCursorPosition(3,9);
+            Console.WriteLine("0 - Exit");
+            Console.SetCursorPosition(3,10);
+            Console.Write("Option: ");
+        }
+
         private static void DrawingHeaderFooter()
         {
             Console.Write("+");
