593b52c1677bc040880a225f3ff4037dfc1ad737
Thiago <thagocruz@gmail.com>
2020-09-20 17:25:44 -0300

    Create Editor.cs

---
+++ HtmlEditor/Editor.cs
@@ -0,0 +1,15 @@
+namespace HtmlEditor
+{
+    public static class Editor
+    {
+        public static void Show()
+        {
+            
+        }
+
+        public static void Start()
+        {
+
+        }
+    }
+}
