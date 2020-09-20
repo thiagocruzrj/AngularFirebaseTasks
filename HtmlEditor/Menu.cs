35e5301caa35c01467cf6e0a665a439511ff281f
Thiago <thagocruz@gmail.com>
2020-09-20 16:33:29 -0300

    Drawing lines

---
+++ HtmlEditor/Menu.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace HtmlEditor
+{
+    public static class Menu
+    {
+        public static void Show()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+
+        public static void DrawScreen()
+        {
+            Console.Write("+");
+            for(int i = 0; i <= 30; i++)
+                Console.Write("-");
+            
+            Console.Write("+");
+            Console.Write("\n");
+        }
+    }
+}
