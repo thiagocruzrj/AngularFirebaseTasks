6a2b6d9892bdb7927abe110a145ef297880a6276
Thiago <thagocruz@gmail.com>
2020-09-20 16:46:53 -0300

    Add drawing screen method

--- HtmlEditor/Menu.cs
+++ HtmlEditor/Menu.cs
@@ -9,9 +9,27 @@ namespace HtmlEditor
             Console.Clear();
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.ForegroundColor = ConsoleColor.Black;
+            DrawScreen();
+            WriteOptions();
         }
 
         public static void DrawScreen()
+        {
+            DrawingHeaderFooter();
+
+            for (int lines = 0; lines <= 10; lines++)
+            {
+                Console.Write("|");
+                for (int i = 0; i <= 30; i++)
+                    Console.Write(" ");
+
+                Console.Write("|");
+                Console.Write("\n");
+            }
+            DrawingHeaderFooter();
+        }
+
+        private static void DrawingHeaderFooter()
         {
             Console.Write("+");
             for(int i = 0; i <= 30; i++)
