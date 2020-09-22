6f3e15d8d4d8964a3f1f1ed3976b7f1aa9ab37fa
Thiago <thagocruz@gmail.com>
2020-09-21 21:42:42 -0300

    starting date formatter

--- MyApp2/Program.cs
+++ MyApp2/Program.cs
@@ -7,15 +7,10 @@ namespace MyApp2
         static void Main(string[] args)
         {
             Console.Clear();
-            var data = new DateTime(2020, 10, 12, 8, 23, 14);
-            //var actualDatetime = DateTime.Now;
-            Console.WriteLine(data);
-            Console.WriteLine(data.Year);
-            Console.WriteLine(data.Month);
-            Console.WriteLine(data.Day);
-            Console.WriteLine(data.Hour);
-            Console.WriteLine(data.Minute);
-            Console.WriteLine(data.Second);
+            var data = DateTime.Now;
+
+            var format = string.Format("{0}", data);
+            Console.WriteLine(format);
         }
     }
 }
