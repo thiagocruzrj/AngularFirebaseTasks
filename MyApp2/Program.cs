513e53aed88d4cbd83513f74638911feec18f9ba
Thiago <thagocruz@gmail.com>
2020-09-21 21:33:27 -0300

    Add more properties

--- MyApp2/Program.cs
+++ MyApp2/Program.cs
@@ -10,6 +10,12 @@ namespace MyApp2
             var data = new DateTime(2020, 10, 12, 8, 23, 14);
             //var actualDatetime = DateTime.Now;
             Console.WriteLine(data);
+            Console.WriteLine(data.Year);
+            Console.WriteLine(data.Month);
+            Console.WriteLine(data.Day);
+            Console.WriteLine(data.Hour);
+            Console.WriteLine(data.Minute);
+            Console.WriteLine(data.Second);
         }
     }
 }
