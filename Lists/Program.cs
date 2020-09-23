9d777b097b394f2ca68fcbeef0e8d6e7ea3bb2e3
Thiago <thagocruz@gmail.com>
2020-09-22 22:46:36 -0300

    creating new array

---
+++ Lists/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            var newArray = new int[5];
+            newArray[0] = 10;
+
+            Console.WriteLine(newArray[0]);
+        }
+    }
+}
