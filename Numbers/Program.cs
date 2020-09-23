266f227c91f4c69b12c0581b2f1a61c76b816697
Thiago <thagocruz@gmail.com>
2020-09-22 22:37:43 -0300

    use some math methods

--- Numbers/Program.cs
+++ Numbers/Program.cs
@@ -12,6 +12,11 @@ namespace Numbers
             Console.WriteLine(value.ToString(CultureInfo.CreateSpecificCulture("en-US")));
             Console.WriteLine(value.ToString(CultureInfo.CreateSpecificCulture("pt-BR")));
             Console.WriteLine(value.ToString("C ",CultureInfo.CreateSpecificCulture("en-UK")));
+
+            decimal newValue = 1033.6534M;
+            Console.WriteLine(Math.Round(newValue));
+            Console.WriteLine(Math.Ceiling(newValue));
+            Console.WriteLine(Math.Floor(newValue));
         }
     }
 }
