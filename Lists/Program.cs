9eb5c927f94e2409ad534b1eb8483a17998e9b6f
Thiago <thagocruz@gmail.com>
2020-09-22 22:48:09 -0300

    initializing some positions

--- Lists/Program.cs
+++ Lists/Program.cs
@@ -9,9 +9,12 @@ namespace Lists
             Console.Clear();
 
             var newArray = new int[5];
+
+            var newArray2 = new int[5]{1,2,3,4,5};
             newArray[0] = 10;
 
             Console.WriteLine(newArray[0]);
+            Console.WriteLine(newArray2[3]);
         }
     }
 }
