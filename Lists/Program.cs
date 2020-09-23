eb73046c74dfaabb97ee8bb685c6c4d230f48fff
Thiago <thagocruz@gmail.com>
2020-09-22 22:51:07 -0300

    Add new struct array

--- Lists/Program.cs
+++ Lists/Program.cs
@@ -11,10 +11,18 @@ namespace Lists
             var newArray = new int[5];
 
             var newArray2 = new int[5]{1,2,3,4,5};
+
+            var newStructArray = new Test[3]{new Test(), new Test(), new Test()};
             newArray[0] = 10;
 
             Console.WriteLine(newArray[0]);
             Console.WriteLine(newArray2[3]);
+            Console.WriteLine(newStructArray[3]);
+        }
+
+        struct Test
+        {
+            public int Id { get; set; }
         }
     }
 }
