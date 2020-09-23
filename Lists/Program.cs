5ee93743916b37d89def24a50a3da832cb337c4b
Thiago <thagocruz@gmail.com>
2020-09-22 23:01:20 -0300

    Show array items with .length

--- Lists/Program.cs
+++ Lists/Program.cs
@@ -10,14 +10,21 @@ namespace Lists
 
             var newArray = new int[5];
 
-            var newArray2 = new int[5]{1,2,3,4,5};
+            var newArray2 = new int[5] { 1, 2, 3, 4, 5 };
 
             var newStructArray = new Test[3]{new Test(), new Test(), new Test()};
             newArray[0] = 10;
 
             Console.WriteLine(newArray[0]);
             Console.WriteLine(newArray2[3]);
+            Console.WriteLine(newArray2.Length);
+
             Console.WriteLine(newStructArray[3]);
+
+            for (var index = 0; index < newArray2.Length ; index++)
+            {
+                Console.WriteLine(newArray2[index]);
+            }
         }
 
         struct Test
